f5b1b734a73dc780e0811bf4d1c4172a7bb9b78b
root <lakshmid@brightcomgroup>
2023-03-05 12:15:54 +0000

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,20 @@
-export default function cleanSet(set, startString) {
+function cleanSet(set, startString) {
   const string = [];
 
-  if (typeof startString !== 'string' || typeof set !== 'object' || startString.length === 0) {
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
   }
+
   for (const item of set) {
-    string.push(item.slice(startString.length));
+    if (item && item.startsWith(startString)) {
+      string.push(item.slice(startString.length));
+    }
   }
   return string.join('-');
 }
+
+export default cleanSet;
